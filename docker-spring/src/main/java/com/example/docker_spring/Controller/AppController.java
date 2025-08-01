59d36e97303710789bb35d86a4542b1b920a2b84
Rosy <rosy.jihye.noh@gmail.com>
2025-08-01 16:16:51 +0900

    Second commit: Docker 연습 프로젝트 업로드

--- docker-spring/src/main/java/com/example/docker_spring/Controller/AppController.java
+++ docker-spring/src/main/java/com/example/docker_spring/Controller/AppController.java
@@ -1,4 +1,4 @@
-package com.example.docker_spring.controller;
+package com.example.docker_spring.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
